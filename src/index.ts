197a6fd857833b6687261d8e0cac3033d01cef4f
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-12-15 18:26:58 +0330

    feat: add request to leetcode

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,29 @@ bot.onText(/\/start/, async (message, match) => {
     type: 'regular',
   });
 });
+
+type LeetCodeQuestion = {
+  'title-slug': string;
+};
+
+type LeetCodeResponse = {
+  data: {
+    problemsetQuestionList: { total: number; questions: LeetCodeQuestion[] };
+  };
+};
+
+const limit = 1000;
+const difficulty = 'EASY';
+
+fetch('https://leetcode.com/graphql/', {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: `{"query":"\\n    query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {\\n  problemsetQuestionList: questionList(\\n    categorySlug: $categorySlug\\n    limit: $limit\\n    skip: $skip\\n    filters: $filters\\n  ) {\\n    total: totalNum\\n    questions: data {\\n      acRate\\n      difficulty\\n      freqBar\\n      frontendQuestionId: questionFrontendId\\n      isFavor\\n      paidOnly: isPaidOnly\\n      status\\n      title\\n      titleSlug\\n      topicTags {\\n        name\\n        id\\n        slug\\n      }\\n      hasSolution\\n      hasVideoSolution\\n    }\\n  }\\n}\\n    ","variables":{"categorySlug":"algorithms","skip":0,"limit":${limit},"filters":{"difficulty":"${difficulty}"}},"operationName":"problemsetQuestionList"}`,
+  method: 'POST',
+  credentials: 'include',
+})
+  .then((res) => res.json())
+  .then((data: LeetCodeResponse) =>
+    console.log(data.data.problemsetQuestionList.questions.length),
+  );
