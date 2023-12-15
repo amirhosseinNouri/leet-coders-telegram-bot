bde3e60314d048fd31a8580000fee256a33b892d
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2023-12-15 18:08:29 +0330

    feat: implement a simple telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
 
 dotenv.config();
 
-console.log('start');
+const bot = new TelegramBot(String(process.env.TELEGRAM_API_TOKEN), {
+  polling: true,
+});
+
+bot.setMyCommands([{ command: '/start', description: 'start the bot' }]);
+
+bot.onText(/\/start/, async (message, match) => {
+  const chatId = message.chat.id;
+  bot.sendPoll(chatId, "Did you solve today's question?", ['Yes ✅', 'No ❌'], {
+    is_anonymous: false,
+    type: 'regular',
+  });
+});
